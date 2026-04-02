6484e9719fba2ab5d477bea66c1d9dd344d90504
Griffin <33357138+DGriffin91@users.noreply.github.com>
2026-04-01 18:21:42 -0700

    rat

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,93 @@
+use std::f32::consts::PI;
+
+use bevy::anti_alias::fxaa::Fxaa;
+use bevy::anti_alias::taa::TemporalAntiAliasing;
 use bevy::asset::AssetMetaCheck;
+use bevy::camera::Hdr;
+use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin};
+use bevy::light::CascadeShadowConfigBuilder;
+use bevy::pbr::{ContactShadows, ScreenSpaceAmbientOcclusion};
+use bevy::post_process::bloom::Bloom;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
             meta_check: AssetMetaCheck::Never,
             ..default()
         }))
+        .add_plugins(FreeCameraPlugin)
+        .insert_resource(GlobalAmbientLight::NONE)
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
-    commands.spawn(Sprite {
-        image: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(SceneRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("rat1.glb")),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(500.0, 500.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.001, 0.05, 0.001))),
+    ));
+
+    // camera
+    let mut camera_emcds = commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        FreeCamera {
+            walk_speed: 10.0,
+            run_speed: 20.0,
+            ..default()
+        },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 1000.0,
+            ..default()
+        },
+        Hdr,
+        Bloom {
+            intensity: 0.1,
+            ..default()
+        },
+    ));
+
+    #[cfg(not(target_arch = "wasm32"))]
+    camera_emcds.insert((
+        Msaa::Off,
+        Fxaa::default(),
+        ContactShadows::default(),
+        ScreenSpaceAmbientOcclusion::default(),
+        TemporalAntiAliasing::default(),
+    ));
+
+    #[cfg(target_arch = "wasm32")]
+    camera_emcds.insert(Fxaa::default());
+
+    commands.spawn((
+        Transform::from_rotation(Quat::from_euler(EulerRot::XYZ, PI * -0.35, PI * -0.13, 0.0)),
+        DirectionalLight {
+            color: Color::srgb(1.0, 0.87, 0.78),
+            illuminance: 5000.0,
+            shadow_maps_enabled: true,
+            contact_shadows_enabled: true,
+            shadow_depth_bias: 0.03,
+            shadow_normal_bias: 0.05,
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            num_cascades: 2,
+            minimum_distance: 0.05,
+            maximum_distance: 30.0,
+            first_cascade_far_bound: 5.0,
+            overlap_proportion: 0.2,
+        }
+        .build(),
+    ));
 }
